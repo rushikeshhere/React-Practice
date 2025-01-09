750be0a8343f8584e7a117002a8c645d70fb0330
Rushikesh Here <rushikeshhere@gmail.com>
2025-01-09 22:22:54 +0530

    Learnt state management

--- 4 Fragments/Learning-fragments-Props-Children/src/App.jsx
+++ 4 Fragments/Learning-fragments-Props-Children/src/App.jsx
@@ -3,16 +3,27 @@ import ErrorMessage from "./components/ErrorMessage";
 import FoodItems from "./components/FoodItems";
 import "./App.css";
 import Container from "./components/Container";
+import FoodInput from "./components/FoodInput";
+import { useState } from "react";
 
 function App() {
   let foodItems = ["Dal", "Roti", "Milk", "Rice", "Paneer", "Biryani"];
   //let foodItems = [];
+
+  let [foodItem, setFoodItem] = useState(" ");
+  let handleOnChangeTest = (event) => {
+    console.log(event.target.value);
+    let value = event.target.value;
+    setFoodItem(value);
+  };
 
   return (
     <>
       <Container>
         <h1 style={{ textAlign: "center" }}> List of Healthy Foods</h1>
         <ErrorMessage items={foodItems} />
+        <FoodInput handleOnChangeEvent={handleOnChangeTest}></FoodInput>
+        <p>{foodItem}</p>
         <FoodItems item={foodItems}></FoodItems>
       </Container>
 
