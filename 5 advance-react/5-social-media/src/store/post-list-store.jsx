be9df54264e8ab56b63b9d6b944f1548852f0cc0
Rushikesh Here <rushikeshhere@gmail.com>
2025-01-24 22:44:10 +0530

    added logic to add multiple post using fetch api but unable to take data using this api! trying to solve...

---
+++ 5 advance-react/5-social-media/src/store/post-list-store.jsx
@@ -0,0 +1,68 @@
+import { createContext, useReducer } from "react";
+
+export const PostList = createContext({
+  postList: [],
+  addPost: () => {},
+  addInitialPosts: () => {},
+  deletePost: () => {},
+});
+
+const postListReducer = (currentPostList, action) => {
+  let newPostList = currentPostList;
+  if (action.type === "DELETE_POST") {
+    newPostList = currentPostList.filter((post) => post.id !== action.payload);
+  } else if (action.type === "ADD_INITIAL_POSTS") {
+    newPostList = action.payload.posts;
+  } else if (action.type === "ADD_POST") {
+    newPostList = [action.payload, ...currentPostList];
+  }
+  return newPostList;
+};
+
+const PostListProvider = ({ children }) => {
+  const [postList, dispatchPostList] = useReducer(postListReducer, []);
+
+  const addPost = (author, user_id, title, content, date, tags) => {
+    dispatchPostList({
+      type: "ADD_POST",
+      payload: {
+        id: Date.now(),
+        author,
+        user_id,
+        title,
+        content,
+        date,
+        tags,
+      },
+    });
+  };
+
+  const addInitialPosts = (posts) => {
+    dispatchPostList({
+      type: "ADD_INITIAL_POSTS",
+      payload: posts,
+    });
+  };
+
+  const deletePost = (postId) => {
+    dispatchPostList({
+      type: "DELETE_POST",
+      payload: postId,
+    });
+  };
+
+  return (
+    <PostList.Provider
+      value={{
+        postList,
+        addPost,
+        addInitialPosts,
+        deletePost,
+      }}
+    >
+      {children}
+    </PostList.Provider>
+  );
+};
+
+export default PostListProvider;
