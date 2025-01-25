c16adadb75aef0496450d119ca2cad80417427ed
Rushikesh Here <rushikeshhere@gmail.com>
2025-01-25 22:54:52 +0530

    removed author ,date and added Reaction object in which likes and dislikes are there  and they will in add in card post

--- 5 advance-react/5-social-media/src/components/CreatePost.jsx
+++ 5 advance-react/5-social-media/src/components/CreatePost.jsx
@@ -2,47 +2,37 @@ import { useContext, useRef } from "react";
 import { PostList } from "../store/post-list-store";
 
 const CreatePost = () => {
-  const authorElement = useRef();
+  const { addPost } = useContext(PostList);
   const user_id_Element = useRef();
   const titleElement = useRef();
   const contentElement = useRef();
-  const dateElement = useRef();
   const tagsElement = useRef();
-
-  const { addPost } = useContext(PostList);
+  const likesElement = useRef();
+  const dislikesElement = useRef();
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const author = authorElement.current.value;
+
     const user_id = user_id_Element.current.value;
     const title = titleElement.current.value;
     const content = contentElement.current.value;
-    const date = dateElement.current.value;
+
     const tags = tagsElement.current.value.split(" ");
+    const likes = parseInt(likesElement.current.value, 10);
+    const dislikes = parseInt(dislikesElement.current.value, 10);
 
-    addPost(author, user_id, title, content, date, tags);
+    addPost(user_id, title, content, tags, likes, dislikes);
 
-    authorElement.current.value = "";
     user_id_Element.current.value = "";
     titleElement.current.value = "";
     contentElement.current.value = "";
-    dateElement.current.value = "";
     tagsElement.current.value = "";
+    likesElement.current.value = "";
+    dislikesElement.current.value = "";
   };
 
   return (
     <form className="form-container" onSubmit={handleSubmit}>
-      <div className="mb-3">
-        <label htmlFor="author" className="form-label">
-          Author Name
-        </label>
-        <input
-          type="text"
-          ref={authorElement}
-          className="form-control"
-          id="author"
-        />
-      </div>
       <div className="mb-3">
         <label htmlFor="user_id" className="form-label">
           User Id
@@ -76,17 +66,7 @@ const CreatePost = () => {
           id="user_id"
         />
       </div>
-      <div className="mb-3">
-        <label htmlFor="date" className="form-label">
-          Date
-        </label>
-        <input
-          type="date"
-          ref={dateElement}
-          className="form-control"
-          id="date"
-        />
-      </div>
+
       <div className="mb-3">
         <label htmlFor="tags" className="form-label">
           Use Tags for better search
@@ -97,6 +77,30 @@ const CreatePost = () => {
           className="form-control"
           id="tags"
           placeholder="add tags using spaces"
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="likes" className="form-label">
+          Likes
+        </label>
+        <input
+          type="number"
+          ref={likesElement}
+          className="form-control"
+          id="likes"
+          required
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="dislikes" className="form-label">
+          Dislikes
+        </label>
+        <input
+          type="number"
+          ref={dislikesElement}
+          className="form-control"
+          id="dislikes"
+          required
         />
       </div>
 
