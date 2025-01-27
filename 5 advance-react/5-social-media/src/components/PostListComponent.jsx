ddb6ce3ace5e0bf64c4a3d47a011ebc96a80e5f6
Rushikesh Here <rushikeshhere@gmail.com>
2025-01-27 22:30:48 +0530

    learnt how to use useEffect and added loadingSpinner Component to spin spinner while loading data

--- 5 advance-react/5-social-media/src/components/PostListComponent.jsx
+++ 5 advance-react/5-social-media/src/components/PostListComponent.jsx
@@ -1,28 +1,41 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import Post from "./Post";
 import { PostList } from "../store/post-list-store";
 import WelcomeMessage from "./WelcomeMessage";
+import LoadingSpinner from "./LoadingSpinner";
 
 const PostListComponent = () => {
   const { postList, addInitialPosts } = useContext(PostList);
+  const [intitalLoading, setInitialLoading] = useState(false);
 
-  const [isLoading, setIsLoading] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
 
-  if (!isLoading) {
+  // if (!isLoading) {
+  //   fetch("https://dummyjson.com/posts")
+  //     .then((res) => res.json())
+  //     .then((data) => addInitialPosts(data));
+  //   setIsLoading(true);
+  // }
+
+  useEffect(() => {
+    setInitialLoading(true);
     fetch("https://dummyjson.com/posts")
       .then((res) => res.json())
-      .then((data) => addInitialPosts(data));
-    setIsLoading(true);
-  }
+      .then((data) => {
+        addInitialPosts(data);
+        setInitialLoading(false);
+      });
+  }, []);
 
   const handleGetPosts = () => {};
 
   return (
     <>
       {/* {postList.length === 0 && <WelcomeMessage onGetPosts={handleGetPosts} />} */}
-      {postList.map((post) => (
-        <Post key={post.id} post={post} />
-      ))}
+      {intitalLoading && <LoadingSpinner />}
+      {!intitalLoading && postList.length === 0 && <WelcomeMessage />}
+      {!intitalLoading &&
+        postList.map((post) => <Post key={post.id} post={post} />)}
     </>
   );
 };
