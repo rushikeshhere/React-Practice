da909a36f8c3d6c11f5dbb6a54180b0e419ad839
Rushikesh Here <rushikeshhere@gmail.com>
2025-02-01 23:01:55 +0530

    Loading Spinner .jsx

---
+++ 5 advance-react/6-react-router/src/components/LoadingSpinner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LoadingSpinner = () => {
+  return (
+    <div className="spinner-grow text-primary spinner" role="status">
+      <span className="visually-hidden">Loading...</span>
+    </div>
+  );
+};
+
+export default LoadingSpinner;
