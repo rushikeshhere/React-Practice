26d327c56997503e628275253d5bceb3fe7870e4
Rushikesh Here <rushikeshhere@gmail.com>
2025-02-08 23:03:01 +0530

    removed Redux from the project

--- 6 Redux/Counter-React-Redux/src/components/Container.jsx
+++
@@ -1,8 +0,0 @@
-const Container = ({ children }) => {
-  return (
-    <center className="card cardContiner" style={{ width: "70%" }}>
-      <div className="card-body">{children}</div>
-    </center>
-  );
-};
-export default Container;
--- 6 Redux/Counter-React-Redux/src/components/Header.jsx
+++
@@ -1,4 +0,0 @@
-const Header = () => {
-  return <h1>Counter</h1>;
-};
-export default Header;
