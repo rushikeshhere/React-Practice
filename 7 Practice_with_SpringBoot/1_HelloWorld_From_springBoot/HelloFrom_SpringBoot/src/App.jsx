d650c2816255cc8aab03c9804bd34614c12e0c05
Rushikesh Here <rushikeshhere@gmail.com>
2025-02-10 22:33:33 +0530

    added component in the app.jsx

---
+++ 7 Practice_with_SpringBoot/1_HelloWorld_From_springBoot/HelloFrom_SpringBoot/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Hello_world from "./components/Hello_world";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
