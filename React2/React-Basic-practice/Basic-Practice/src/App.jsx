45df365c2ed9c1bcf5ac8df23ac59e99671f5388
Rushikesh Here <rushikeshhere@gmail.com>
2025-02-11 22:48:08 +0530

    app.jsx to render other components

---
+++ React2/React-Basic-practice/Basic-Practice/src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import Five from "./components/Five";
+import Four from "./components/Four";
+import { One } from "./components/One";
+import Three from "./components/Three";
+import Two from "./components/Two";
+
+function App() {
+  return (
+    <>
+      {/* <One></One> */}
+      {/* <Two /> */}
+      {/* <Three /> */}
+      {/* <Four /> */}
+      <Five />
+    </>
+  );
+}
+
+export default App;
