b586f545eb1e18ee127b74221bdf706c531e9eaf
Rushikesh Here <rushikeshhere@gmail.com>
2025-02-14 23:28:11 +0530

    app.jsx

--- React2/React-Basic-practice/Basic-Practice/src/App.jsx
+++ React2/React-Basic-practice/Basic-Practice/src/App.jsx
@@ -1,18 +1,31 @@
 import "./App.css";
 import Eight from "./components/Eight";
 import Eleven from "./components/Eleven";
+import Fifteen from "./components/Fifteen";
 import Five from "./components/Five";
 import Four from "./components/Four";
+import Fourteen from "./components/Fourteen";
 import Nine from "./components/Nine";
 import { One } from "./components/One";
 import Seven from "./components/Seven";
 import Six from "./components/Six";
 import Ten from "./components/Ten";
 import Three from "./components/Three";
+import Thrteen from "./components/Thrteen";
 import Twelve from "./components/Twelve";
 import Two from "./components/Two";
 
 function App() {
+  const itemsList = [
+    "Item1",
+    "Item2",
+    "Item3",
+    "Item4",
+    "New Item1",
+    "New Item2",
+    "New Item3",
+    "New Item4",
+  ];
   return (
     <>
       {/* <One></One> */}
@@ -26,7 +39,10 @@ function App() {
       {/* <Nine /> */}
       {/* <Ten /> */}
       {/* <Eleven /> */}
-      <Twelve />
+      {/* <Twelve /> */}
+      {/* <Thrteen /> */}
+      {/* <Fourteen /> */}
+      <Fifteen items={itemsList} />
     </>
   );
 }
